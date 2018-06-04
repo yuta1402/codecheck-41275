b914e3f6115234f5217e199a0d1871a46a87c7b1
gut <yuta1402souja@live.jp>
2018-06-05 00:31:40 +0900

    Hello, <string>!を出力するように変更

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // start from 1 to ignore script name; argv[0] will be a name of processing file.
-  for (int i = 1; i < argc; i++) {
-    printf ("argv[%i]: %s\n", i, argv[i]);
-  }
-  return 0;
+    cout << "Hello, " << argv[1] << "!" << endl;
+    return 0;
 }
